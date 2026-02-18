19f7cf93898fb5f832f6deb50d8cb9374d90ad29
vyronav <vyron@bondaval.com>
2026-02-18 19:41:46 +0000

    Adds comment plugin

--- init.lua
+++ init.lua
@@ -309,6 +309,33 @@ require('lazy').setup({
     event = 'InsertEnter',
     config = function() require('nvim-autopairs').setup {} end,
   },
+  -- Commenting
+  {
+    'numToStr/Comment.nvim',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    config = function()
+      require('Comment').setup {
+        pre_hook = function(ctx)
+          local U = require 'Comment.utils'
+
+          -- Determine the location for ts_context_commentstring
+          local location = nil
+          if ctx.ctype == U.ctype.block then
+            location = require('ts_context_commentstring.utils').get_cursor_location()
+          elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            location = require('ts_context_commentstring.utils').get_visual_start_location()
+          end
+
+          return require('ts_context_commentstring.internal').calculate_commentstring {
+            key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
+            location = location,
+          }
+        end,
+      }
+    end,
+  },
 }, {
   ui = { icons = { cmd = '⌘', config = '🛠', ft = '📂', plugin = '🔌', start = '🚀' } },
 })
